c9efdd58e8817f5702946315f47d1b5975a360d0
Sam Damazio <128763505+samdamazio@users.noreply.github.com>
2025-05-20 00:03:08 +0000

    resolve race conditions on script run dev

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,20 @@
+const { exec } = require("node:child_process");
+// import com require pq nao vai ser transpilado
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout, stderr) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    } else {
+      console.log("\n\n✅ Postgres pronto para fazer conexões\n");
+      return;
+    }
+  }
+}
+
+console.log("\n\n🔴 Aguardando Postgres aceitar conexões");
+checkPostgres();
