4995b4d34f22acdfc5088bf958437e6ed845b031
Sam Damazio <128763505+samdamazio@users.noreply.github.com>
2025-05-22 15:23:26 +0000

    feat: create `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,53 @@
+import { resolve } from "node:path";
+import database from "infra/database.js";
+import migrationRunner from "node-pg-migrate";
+
+const defaultMigrationOptions = {
+  // databaseUrl: process.env.DATABASE_URL, outra forma de passar a conexão
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  dryRun: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+      dryRun: false,
+    });
+
+    return migratedMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
