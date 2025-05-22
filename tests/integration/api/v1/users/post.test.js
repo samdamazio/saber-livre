fa305891a49ae2179b189e3f6973f259c6074129
samdamazio <samuel_damazio@hotmail.com>
2025-05-22 15:44:10 -0300

    test: adds post tests for api users

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,14 +1,30 @@
 import orchestrator from "tests/orchestrator.js";
+import database from "infra/database.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await orchestrator.clearDatabase();
-  await orchestrator.runPedingMigrations();
+  await orchestrator.runPendingMigrations();
 });
 
 describe("POST /api/v1/users", () => {
   describe("Anonymous user", () => {
     test("With unique and valid data", async () => {
+      await database.query({
+        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
+        values: ["testuser", "testemail@gmail.com", "senha123"],
+      });
+
+      await database.query({
+        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
+        values: ["testuser2", "Testemail@gmail.com", "senha123"],
+      });
+
+      const users = await database.query({
+        text: "SELECT * FROM users",
+      });
+      console.log(users.rows);
+
       const response = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
       });
