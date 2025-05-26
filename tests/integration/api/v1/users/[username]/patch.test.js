76de01e6de03539b08085d0fd588646d8969536b
samdamazio <samuel_damazio@hotmail.com>
2025-05-26 13:22:42 -0300

    feat: add update function to a user

---
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -0,0 +1,246 @@
+import { version as uuidVersion } from "uuid";
+import orchestrator from "tests/orchestrator.js";
+import user from "models/user.js";
+import password from "models/password.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("PATCH /api/v1/users/[username]", () => {
+  describe("Anonymous user", () => {
+    test("With nonexistent 'username'", async () => {
+      const response = await fetch(
+        "http://localhost:3000/api/v1/users/nonexistentuser",
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+
+      expect(response.status).toBe(404);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "NotFoundError",
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+        status_code: 404,
+      });
+    });
+
+    test("With duplicated 'username'", async () => {
+      const user1Response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "user1",
+          email: "user1email@ceasinha.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(user1Response.status).toBe(201);
+
+      const user2Response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "user2",
+          email: "user2email@ceasinha.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(user2Response.status).toBe(201);
+
+      const response = await fetch("http://localhost:3000/api/v1/users/user2", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "user1",
+        }),
+      });
+
+      expect(response.status).toBe(400);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "ValidationError",
+        message: "O username informado já está sendo utilizado.",
+        action: "Utilize outro username para realizar esta operação.",
+        status_code: 400,
+      });
+    });
+
+    test("With duplicated 'email'", async () => {
+      const user1Response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "email1",
+          email: "email1@ceasinha.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(user1Response.status).toBe(201);
+
+      const user2Response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "email2",
+          email: "email2@ceasinha.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(user2Response.status).toBe(201);
+
+      const response = await fetch(
+        "http://localhost:3000/api/v1/users/email2",
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: "email1@ceasinha.com",
+          }),
+        },
+      );
+
+      expect(response.status).toBe(400);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "ValidationError",
+        message: "O email informado já está sendo utilizado.",
+        action: "Utilize outro email para realizar esta operação.",
+        status_code: 400,
+      });
+    });
+
+    test("With unique 'username' and 'email'", async () => {
+      const userResponse = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "uniqueUser",
+          email: "uniqueUser@ceasinha.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(userResponse.status).toBe(201);
+
+      const response = await fetch(
+        "http://localhost:3000/api/v1/users/uniqueUser",
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: "newUniqueUsername",
+            email: "newUniqueEmail@ceasinha.com",
+          }),
+        },
+      );
+
+      const responseBody = await response.json();
+      expect(response.status).toBe(200);
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "newUniqueUsername",
+        email: "newUniqueEmail@ceasinha.com",
+        password: responseBody.password,
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+      expect(responseBody.updated_at > responseBody.created_at).toBe(true);
+    });
+
+    test("With new 'password'", async () => {
+      const userResponse = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "newPasswordUser",
+          email: "newPasswordUser@ceasinha.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(userResponse.status).toBe(201);
+
+      const response = await fetch(
+        "http://localhost:3000/api/v1/users/newPasswordUser",
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            password: "NewPassword123",
+          }),
+        },
+      );
+
+      const responseBody = await response.json();
+      expect(response.status).toBe(200);
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "newPasswordUser",
+        email: "newPasswordUser@ceasinha.com",
+        password: responseBody.password,
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+      expect(responseBody.updated_at > responseBody.created_at).toBe(true);
+
+      const userInDatabase = await user.findOneByUsername("newPasswordUser");
+      const correctPassordMatch = await password.compare(
+        "NewPassword123",
+        userInDatabase.password,
+      );
+      const incorrectPassordMatch = await password.compare(
+        "Senha123",
+        userInDatabase.password,
+      );
+
+      expect(correctPassordMatch).toBe(true);
+      expect(incorrectPassordMatch).toBe(false);
+    });
+  });
+});
