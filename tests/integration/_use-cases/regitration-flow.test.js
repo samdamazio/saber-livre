0c639277f273914d37f39e4f1e6fdf2c2f81e4d4
Sam Damazio <128763505+samdamazio@users.noreply.github.com>
2025-10-24 15:31:51 +0000

    feat: add default feat `read:activation_token` when creating `user`

--- tests/integration/_use-cases/regitration-flow.test.js
+++ tests/integration/_use-cases/regitration-flow.test.js
@@ -1,7 +1,6 @@
-import { version as uuidVersion } from "uuid";
 import orchestrator from "tests/orchestrator.js";
-import user from "models/user.js";
-import password from "models/password.js";
+import activation from "models/activation.js";
+import { act } from "react";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -11,6 +10,8 @@ beforeAll(async () => {
 });
 
 describe("Use case Registration Flow (all successful)", () => {
+  let createUserResponseBody;
+
   test("Create user account", async () => {
     const createUserResponse = await fetch(
       "http://localhost:3000/api/v1/users",
@@ -21,7 +22,7 @@ describe("Use case Registration Flow (all successful)", () => {
         },
         body: JSON.stringify({
           username: "RegistrationFlowUser",
-          email: "registration.flow@ceasinha.app.br",
+          email: "registration.flow@saberlivre.com.br",
           password: "RegistrationFlowPassword",
         }),
       },
@@ -29,12 +30,12 @@ describe("Use case Registration Flow (all successful)", () => {
 
     expect(createUserResponse.status).toBe(201);
 
-    const createUserResponseBody = await createUserResponse.json();
+    createUserResponseBody = await createUserResponse.json();
 
     expect(createUserResponseBody).toEqual({
       id: createUserResponseBody.id,
       username: "RegistrationFlowUser",
-      email: "registration.flow@ceasinha.app.br",
+      email: "registration.flow@saberlivre.com.br",
       password: createUserResponseBody.password,
       features: ["read:activation_token"],
       created_at: createUserResponseBody.created_at,
@@ -42,7 +43,21 @@ describe("Use case Registration Flow (all successful)", () => {
     });
   });
 
-  test("Receive activation email", async () => {});
+  test("Receive activation email", async () => {
+    const lastEmail = await orchestrator.getLastEmail();
+
+    const activationToken = await activation.findOneByUserId(
+      createUserResponseBody.id,
+    );
+
+    expect(lastEmail.sender).toBe("<contato@saberlivre.com.br>");
+    expect(lastEmail.recipients[0]).toBe(
+      "<registration.flow@saberlivre.com.br>",
+    );
+    expect(lastEmail.subject).toBe("Ative sua conta no Saber Livre!");
+    expect(lastEmail.text).toContain("RegistrationFlowUser");
+    expect(lastEmail.text).toContain(activationToken.id);
+  });
 
   test("Active account", async () => {});
   test("Login", async () => {});
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -71,6 +71,10 @@ async function getLastEmail() {
   const emailListBody = await emailListResponse.json();
   const lastEmailItem = emailListBody.pop();
 
+  if (!lastEmailItem) {
+    return null;
+  }
+
   const emailTextResponse = await fetch(
     `${emailHttpURL}/messages/${lastEmailItem.id}.plain`,
   );
