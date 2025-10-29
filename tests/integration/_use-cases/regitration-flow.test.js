c506ee5b9cd3f75924b0e636a414809e9e3ff8ae
Sam Damazio <128763505+samdamazio@users.noreply.github.com>
2025-10-29 01:24:51 +0000

    feat: add `inject anonymous or user` and `can request` middlewares to `api/v1/sessions`

--- tests/integration/_use-cases/regitration-flow.test.js
+++ tests/integration/_use-cases/regitration-flow.test.js
@@ -86,6 +86,29 @@ describe("Use case Registration Flow (all successful)", () => {
     expect(activatedUser.features).toEqual(["create:session"]);
   });
 
-  test("Login", async () => {});
+  test("Login", async () => {
+    const createSessionsResponse = await fetch(
+      "http://localhost:3000/api/v1/sessions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "registration.flow@saberlivre.com.br",
+          password: "RegistrationFlowPassword",
+        }),
+      },
+    );
+
+    expect(createSessionsResponse.status).toBe(201);
+
+    const createSessionsResponseBody = await createSessionsResponse.json();
+
+    expect(createSessionsResponseBody.user_id).toEqual(
+      createUserResponseBody.id,
+    );
+  });
+
   test("Get user information", async () => {});
 });
