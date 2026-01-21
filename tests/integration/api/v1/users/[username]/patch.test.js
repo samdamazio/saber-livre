ea7b7b4e4ce90ec5297110b5edd73d0fccef4048
samdamazio <samuel_damazio@hotmail.com>
2026-01-20 21:37:14 -0300

    feat: consider `resource` in `authorization` model

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -69,6 +69,44 @@ describe("PATCH /api/v1/users/[username]", () => {
       });
     });
 
+    test("With `userB` targeting `userA`", async () => {
+      await orchestrator.createUser({
+        username: "userA",
+      });
+
+      const createdUser2 = await orchestrator.createUser({
+        username: "userB",
+      });
+
+      const activatedUser2 = await orchestrator.activateUser(createdUser2);
+      const sessionObject2 = await orchestrator.createSession(
+        activatedUser2.id,
+      );
+
+      const response = await fetch("http://localhost:3000/api/v1/users/userA", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session_id=${sessionObject2.token}`,
+        },
+        body: JSON.stringify({
+          username: "userC",
+        }),
+      });
+
+      expect(response.status).toBe(403);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        action:
+          "Verifique se você possui a feature necessária para atualizar outro usuário.",
+        message: "Você não possui permissão para atualizar outro usuário.",
+        name: "ForbiddenError",
+        status_code: 403,
+      });
+    });
+
     test("With duplicated 'username'", async () => {
       await orchestrator.createUser({
         username: "user1",
@@ -175,7 +213,7 @@ describe("PATCH /api/v1/users/[username]", () => {
         username: "newUniqueUsername",
         email: "newUniqueEmail@ceasinha.com",
         password: responseBody.password,
-        features: ["create:session", "read:session","update:user"],
+        features: ["create:session", "read:session", "update:user"],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -214,7 +252,7 @@ describe("PATCH /api/v1/users/[username]", () => {
         username: activatedUser.username,
         email: activatedUser.email,
         password: responseBody.password,
-        features: ["create:session", "read:session","update:user"],
+        features: ["create:session", "read:session", "update:user"],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -224,7 +262,9 @@ describe("PATCH /api/v1/users/[username]", () => {
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
       expect(responseBody.updated_at > responseBody.created_at).toBe(true);
 
-      const userInDatabase = await user.findOneByUsername(activatedUser.username);
+      const userInDatabase = await user.findOneByUsername(
+        activatedUser.username,
+      );
       const correctPassordMatch = await password.compare(
         "NewPassword2",
         userInDatabase.password,
